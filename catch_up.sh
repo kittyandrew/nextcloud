510ff687b6eb745cebeccd34b86187aa13e75971
kittyandrew <kitty@maudrew.dev>
2020-10-10 21:29:20 +0200

    remove direct https link

--- catch_up.sh
+++ catch_up.sh
@@ -1,4 +1,10 @@
+if [ -z "$1" ]
+  then
+    echo "You have to supply your nextcloud url (like https://example.com/cron.php) as argument."
+    exit 1
+fi
+
 while true; do
-    curl https://nc.kittyandrew.dev/cron.php
+    curl $1
     sleep 5
 done
